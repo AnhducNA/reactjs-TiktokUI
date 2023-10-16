9cb9c5e7071b4fa4ec1b303718b1efb0ac893a1a
AnhducNA <ducank961@gmail.com>
2023-10-16 21:17:44 +0700

    Bài 61: Cấu hình sử dụng CSS/SASS

--- src/alertButton.js
+++
@@ -1,7 +0,0 @@
-function AlertButton() {
-    return ( 
-        alert(123465)
-     );
-}
-
-export default AlertButton;
--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import AlertButton from "~/alertButton";
-
-function Button() {
-    return ( 
-        <button onClick={AlertButton}>Click</button>
-     );
-}
-
-export default Button;
