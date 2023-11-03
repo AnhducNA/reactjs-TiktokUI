29062b47cdcf0985e43f2f8b7ac54e4c06f6764e
AnhducNA <ducank961@gmail.com>
2023-11-03 16:38:54 +0700

    Bài 62: Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {publicRoutes} from "~/routes";
+import DefaultLayout from "~/components/Layout/DefaultLayout";
+import {Fragment} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Home page</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        Layout = Fragment;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page/>
+                                    </Layout>
+                                }
+                            />
+                        )
+                    })}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
